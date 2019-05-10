1174fd1aef169caa4638c9853ddab8f6321eedbd
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-10 08:25:18 -0600

    Adds POST for new recipes

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var recipesController = require('../controllers/recipesController')
-
-router.get('/', recipesController.calories);
-
-module.exports = router;
