4f49a7fd532cae157620bbe2b2cc8d85033bc837
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-09 19:18:17 -0600

    Updated index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var recipesController = require('../controllers/recipesController')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', recipesController.calories);
 
 module.exports = router;
